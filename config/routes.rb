1644444688cc134eb2d1f91ab65e0fe261a18289
Luis Carlos González Hernández <lcarlosglez@gmail.com>
2022-02-21 19:56:47 -0600

    Adds controllers with specifics verbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :turns, only: [:create]
+  resources :players, only: [:create]
+  resources :games, only: [:create, :show, :new]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
